67dabce8a38abde927e755296a8b89331c01f52c
Gustav Pursche <gustavpursche@gmail.com>
2014-09-06 12:37:14 +0200

    prepare first deploy: add coffee-script as dependency, remove hamster-migration

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-var coffeescript = require('coffee-script').register();
+require('coffee-script').register();
 
 Sandglass = require( './sandglass/app.coffee' );
 
 new Sandglass()
-  .start()
-  .then(function( sandglass ) {
-    sandglass.migrate( 'gustavpursche@gmail.com',
-                       'test',
-                       '/Users/gustavpursche/Desktop/hamster.db' )
-  });
+  .start();
